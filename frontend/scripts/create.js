09bffe70692fbc39e519dfc399126eb05771a3a8
ZHENK <chengzhenyang@gmail.com>
2021-09-22 17:49:15 +1200

    forgot to commit this

--- frontend/scripts/create.js
+++ frontend/scripts/create.js
@@ -12,6 +12,9 @@ form.addEventListener("submit", async (e) => {
     await fetch(url, {
         method: "POST",
         mode: "cors",
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(data),
     });
 
